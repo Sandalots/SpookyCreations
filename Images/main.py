47c136c0a9287afe6c5021ed948e6ee477162250
Sandy MacDonald <59518103+Sandalots@users.noreply.github.com>
2022-07-20 21:42:17 +0100

    added new viewing script

---
+++ Images/main.py
@@ -0,0 +1,13 @@
+import os
+import random
+from PIL import Image
+
+path = './images'  # in case the folder is in the same directory as the script
+files = os.listdir(path)  # listdir gives you a list with all filenames in the provided path.
+randomFile = random.choice(files)
+if randomFile == "main.py":
+    randomFile = random.choice(files) # random.choice then selects a random file from your list
+print(randomFile) #  prints the random filename
+
+image = Image.open(path + '/' + randomFile)  # displayed the image
+image.show()
