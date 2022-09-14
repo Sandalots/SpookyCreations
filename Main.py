0c268a65d729f6a1c6ad560b468976a5f1ba557f
Sandy MacDonald <59518103+Sandalots@users.noreply.github.com>
2022-09-14 21:51:36 +0100

    Capital

--- Main.py
+++ Main.py
@@ -5,8 +5,10 @@ from PIL import Image
 path = './images'  # in case the folder is in the same directory as the script
 files = os.listdir(path)  # listdir gives you a list with all filenames in the provided path.
 randomFile = random.choice(files)
-if randomFile == "main.py":
+
+if randomFile == "Main.py":
     randomFile = random.choice(files) # random.choice then selects a random file from your list
+
 print(randomFile) #  prints the random filename
 
 image = Image.open(path + '/' + randomFile)  # displayed the image
